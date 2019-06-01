6adef05ec2a59de720013a5fda6a724f32aeb504
APabloJ <altisimop@gmail.com>
2019-06-01 14:59:18 -0500

    added jQuery to script.js. the Landing page now has active Smooth Scroll via jQuery.

---
+++ script.js
@@ -0,0 +1,12 @@
+// Grabing links in the navbar
+$('.navbar a').on('click', function(e){
+  if (this.hash !== ''){
+    e.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 800);
+  }
+})
